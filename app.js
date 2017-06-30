72d1791796114c5807156bb844571cfe34ee7c71
Prerana Kaila <Prerana Kaila>
2017-06-30 13:59:54 +0530

    code updated

--- app.js
+++ app.js
@@ -129,8 +129,24 @@ var UIController=(function()
         incomeLabel: '.budget__income--value',
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
-        container: '.container'
+        container: '.container',
+        expensesPercLabel:'.item__percentage'
 	};
+    var formatNumber = function(num,type)
+    {var numSplit,int,dec;/*
+    + or - before number
+    exactly 2 decimal points
+    comma separating the thousands
+     */
+     num = Math.abs(num);
+     num = num.toFixed(2);
+     numSplit = num.split(".");
+     int = numSplit[0];
+     if(int.length>3)
+        {int = int.substr(0,int.length-3) + "," + int.substr(int.length-3,3);}
+     dec = numSplit[1];
+     return (type === "exp" ? "-":"+") + " " + int + "." + dec;
+ };
 		return{getInput:function()
 		{return{type:document.querySelector(DOMstrings.inputType).value,
 		description:document.querySelector(DOMstrings.inputDescription).value,
@@ -151,7 +167,7 @@ var UIController=(function()
 		//2 replace placeholder text with some actual data
 		newHtml = html.replace("%id%",obj.id);
 		newHtml = newHtml.replace("%description%",obj.description);
-		newHtml = newHtml.replace("%value%",obj.value);
+		newHtml = newHtml.replace("%value%",formatNumber(obj.value,type));
 		//3 insert the html in the DOM
 		document.querySelector(element).insertAdjacentHTML('beforeend',newHtml);
 	},
@@ -168,11 +184,33 @@ var UIController=(function()
 		fieldsArr[0].focus();
 	},
     displayBudget: function(obj){
-        document.querySelector(DOMstrings.budgetLabel).textContent=obj.budget;
-        document.querySelector(DOMstrings.incomeLabel).textContent=obj.totalInc;
-        document.querySelector(DOMstrings.expensesLabel).textContent=obj.totalExp;
-        document.querySelector(DOMstrings.percentageLabel).textContent=obj.percentage;
+        var type;
+        obj.budget > 0 ? type='inc' : type='exp';
+        document.querySelector(DOMstrings.budgetLabel).textContent= formatNumber(obj.budget,type);
+        document.querySelector(DOMstrings.incomeLabel).textContent= formatNumber(obj.totalInc,'inc');
+        document.querySelector(DOMstrings.expensesLabel).textContent= formatNumber(obj.totalExp,'exp');
+        if(obj.percentage>0)
+        {
+            document.querySelector(DOMstrings.percentageLabel).textContent=obj.percentage + "%";
+        }
+            else
+            {
+                document.querySelector(DOMstrings.percentageLabel).textContent="---"
+            }
+        
     },
+    displayPercentages:function(percentages)
+    {var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+        var nodeListForEach=function(list,callback)
+        {for(var i=0;i<list.length;i++)
+            {callback(list[i],i);}};
+            nodeListForEach(fields,function(current,index)
+                {if(percentages[index]>0)
+                    {current.textContent=percentages[index] + "%";}
+                    else{current.textContent="---";}
+                });
+        },
+        
 	getDOMstrings:function(){return DOMstrings;}
 };
 })();
@@ -202,7 +240,7 @@ var controller=(function(budgetCtrl,UICtrl)
             //2 Read percentages from the budget controller
             var percentages = budgetCtrl.getPercentages();
             //3 Update the UI with the new percentages
-            console.log(percentages);
+            UICtrl.displayPercentages(percentages);
         };
 		var ctrlAddItem=function()
 		{var input,newItem;
